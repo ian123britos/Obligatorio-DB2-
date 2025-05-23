399f73fbb757d9356aabfa9e2bd77a228190968b
Ian Britos Borgunder <IB319561@fi365.ort.edu.uy>
2025-05-23 12:07:35 -0300

    agregamos la query de t-sql terminando los ejercicios de trigger,function,procedure

--- Obligatorio DB2 Consultas.sql
+++ Obligatorio DB2 Consultas.sql
@@ -2,18 +2,43 @@ USE  DeviceRepair
 --a. Para todos los productos existentes, mostrar código y descripción, cantidad de
 --reparaciones con control de calidad realizado, cantidad sin control realizado y
 --cantidad de reparaciones cuyo valor fue superior a $100.
-
+SELECT 
+p.IdProd AS Codigo,
+p.Dscprod AS Descripcion,
+--//conta en el caso cuando idempqa no es null entonces agregale 1 y terminalo
+SUM(Case when r.IdEmpQA is null then 0 else 1 end) as ConControl,
+SUM(Case when r.IdEmpQA IS NULL then 1 else 0 end) as SinControl,
+SUM(Case when r.CostoRepara > 100 then 1 else 0 end) as CantidadMayoresAcien
+FROM Producto p
+inner JOIN Repara r ON R.IdProd = P.IdProd
+GROUP BY P.IdProd , p.Dscprod
 
 
 --b. Muestra los datos del Empleado con mayor cantidad de reparaciones realizadas.
-
+select TOP 1 e.*, COUNT(r.IdRepara) CantidadReparacionesRealizadas
+from Empleado e
+INNER JOIN Repara r ON r.IdEmp = e.IdEmp
+group by e.FchNacEmp,e.IdEmp,e.NomEmp,e.SueldoEmp,e.TipoEmp
+order by CantidadReparacionesRealizadas DESC
 
 --c. Muestra datos del producto y costo total de reparaciones por producto,
 --mostrando solo los productos con un costo total superior a $200.
-
+select DISTINCT p.IdProd,sum(r.CostoRepara)as SumaTotal
+from Producto p
+INNER JOIN Repara r ON r.IdProd = p.IdProd
+group by p.IdProd,r.CostoRepara
+HAVING SUM(r.CostoRepara) > 200
 
 
 --d. Datos del producto más reparado.
+select DISTINCT p.*
+from Producto p
+inner join Repara r ON r.IdProd = p.IdProd
+where p.IdProd IN (
+select top 1 IdProd 
+from Repara
+group by IdProd
+order by count(IdProd) desc)
 
 
 --e. Escribe una consulta que muestre información detallada de los empleados,
@@ -25,12 +50,36 @@ USE  DeviceRepair
 --Ordenar la consulta por salario en orden descendente y, en caso de empate, por
 --el número de reparaciones en orden descendente.
 --Elige los rangos de clasificación de acuerdo con tu criterio (justifica).
-
-
+select e.IdEmp ,e.NomEmp,e.TipoEmp,e.SueldoEmp,
+CASE 
+  WHEN e.SueldoEmp < 15000 THEN 'Bajo' 
+  WHEN e.SueldoEmp BETWEEN 15000 AND 23000 THEN 'Medio'
+  WHEN e.SueldoEmp > 23000 THEN 'Alto'
+END AS ClasificacionSalarial,
+CASE 
+   WHEN E.SueldoEmp > 23000 THEN 'Senior'
+   WHEN E.SueldoEmp BETWEEN 15000 AND 23000 THEN 'Experimentado'
+   ELSE 'Junior'
+END AS NivelSalarial, COUNT(R.IdRepara)AS CantidadReparaciones
+from Empleado e
+left join Repara r on r.IdEmp = e.IdEmp
+GROUP BY e.IdEmp,e.NomEmp,e.TipoEmp,e.SueldoEmp
+ORDER BY e.SueldoEmp DESC,CantidadReparaciones DESC;
+select*from Empleado
+select * from Repara
 
 --f. Muestra el costo total de reparaciones por empleado y un resumen general.
-
+select r.IdEmp,e.NomEmp,COUNT(r.CostoRepara) as cantidadReparaciones, sum(r.CostoRepara) as CostoTotal
+from Repara r
+inner join Empleado e on e.IdEmp = r.IdEmp 
+group by r.IdEmp,e.NomEmp
 
 
 --g. Muestra los datos de los Empleados Técnicos que repararon todos los
 --productos.
+select e.IdEmp
+from Empleado e
+inner join Repara r on r.IdEmp = e.IdEmp
+where TipoEmp = 'T'
+group by e.IdEmp
+HAVING COUNT(DISTINCT(r.IdProd))=(SELECT COUNT(IdProd) FROM Producto)
---
+++ Obligatorio T-SQL.sql
@@ -0,0 +1,154 @@
+
+use DeviceRepair
+
+--4. Utilizando T-SQL realizar los siguientes ejercicios:
+--a. Crea un procedimiento almacenado llamado sp_RegistrarReparacion, que
+--permita registrar una nueva reparación en la tabla Repara, cumpliendo con las
+--siguientes reglas:
+--Parámetros de entrada:
+--@NumSerie (char(10)): Número de serie de la unidad.
+--@IdProd (int): ID del producto asociado.
+--@IdEmp (int): ID del empleado que realiza la reparación.
+--@CostoRepara (money): Costo de la reparación.
+--Validaciones:
+--La unidad (@NumSerie, @IdProd) debe existir en la tabla Unidad.
+--El empleado (@IdEmp) debe existir en la tabla Empleado.
+--Un empleado no puede registrar más de una reparación para la misma unidad en el mismo día.
+--El costo de reparación no puede ser negativo.
+--Operación:
+--Insertar la nueva reparación en la tabla Repara, con el estado 'Iniciado' y la
+--fecha/hora actual.
+--Retornar un mensaje indicando el éxito o el motivo delfallo.
+	CREATE PROCEDURE sp_RegistrarReparacion 
+	@NumSerie char(10),@IdProd int,@IdEmp int,@CostoRepara money
+	AS 
+	BEGIN
+	 SET NOCOUNT ON
+	 IF NOT EXISTS(SELECT 1 FROM Unidad where @NumSerie = NumSerie and @IdProd = IdProd)
+	 BEGIN
+	 PRINT 'La unidad no existe '
+	 RETURN 
+	 END
+	 IF NOT EXISTS(SELECT 1 FROM Empleado where @IdEmp = IdEmp)
+	 BEGIN
+	 PRINT 'El empleado no existe'
+	 RETURN
+	 END
+	 IF(@CostoRepara < 0)
+	 BEGIN
+	 PRINT 'El costo de reparacion no puede ser menor a 0'
+	 RETURN
+	 END
+	 IF EXISTS(SELECT 1 FROM Repara WHERE IdProd = @IdProd AND NumSerie = @NumSerie AND IdEmp = @IdEmp AND DAY(FchRepara) = DAY(GETDATE()))
+	 BEGIN
+	 PRINT 'El empleado ya hizo una reparacion para esa unidad hoy'
+	 END
+	insert into Repara(NumSerie, IdProd ,IdEmp ,FchRepara,CostoRepara)
+	VALUES (@NumSerie,@IdProd,@IdEmp,GETDATE(),@CostoRepara);
+	PRINT 'La reparacion se a insertado correctamente'
+	END;
+	
+
+
+--b. Crea una función escalar llamada fn_CalcularTiempoReparacion, que reciba el
+--número de serie y el ID del producto y devuelva el tiempo total (en días) que ha
+--estado en reparación.
+--Parámetros de entrada
+--@NumSerie (char(10)): Número de serie de la unidad.
+--@IdProd (int): ID del producto.
+--Contar todos los días únicos en los que la unidad ha estado en reparación, sin
+--importar el número de reparaciones en un mismo día.
+--Usar la columna FchRepara de la tabla Repara.
+--Si la unidad no tiene reparaciones registradas, debe devolver NULL.
+--Salida:
+--Un int con la cantidad de días distintos en los que se ha reparado la unidad.
+CREATE FUNCTION fn_CalcularTiempoReparacion 
+(@NumSerie char(10),@IdProd int) RETURNS INT AS 
+BEGIN
+DECLARE @TiempoTotalEnDias int
+SELECT @TiempoTotalEnDias = COUNT(DISTINCT FchRepara) 
+FROM Repara
+WHERE @NumSerie = NumSerie AND @IdProd = IdProd
+IF (@TiempoTotalEnDias = 0)
+BEGIN
+RETURN NULL
+END
+RETURN @TiempoTotalEnDias
+END
+
+--5. Escribir los siguientes disparadores (por supuesto: considerando modificaciones
+--múltiples)
+--a. Crea un disparador llamado trg_ControlEstadoReparacion, que se active
+--cuando se UPDATE la columna StsRepara ON Repara.
+--Eltrigger debe ejecutarse cuando:
+--El estado de reparación (StsRepara) cambie a "Terminado" o "Cancelado".
+--No debe activarse si el estado no cambió.
+--Acciones a realizar:
+--Registrar el cambio en una tabla llamada HistoricoReparacion, que debe
+--crearse con los siguientes campos:
+--CREATE TABLE HistoricoReparacion (
+--IdHist INT IDENTITY PRIMARY KEY,
+--IdRepara INT NOT NULL,
+--NumSerie CHAR(10) NOT NULL,
+--IdProd INT NOT NULL,
+--EstadoAnterior VARCHAR(20) NOT NULL,
+--EstadoNuevo VARCHAR(20) NOT NULL,
+--FchCambio DATETIME DEFAULT GETDATE());
+--Insertar en esta tabla el IdRepara, NumSerie, IdProd, el estado anterior, el
+--estado nuevo y la fecha del cambio.
+
+CREATE TRIGGER trg_ControlEstadoReparacion
+ON Repara 
+AFTER UPDATE
+AS
+BEGIN
+ SET NOCOUNT ON;
+ IF NOT UPDATE(StsRepara)
+ BEGIN
+ RETURN;
+ END
+ INSERT INTO HistoricoReparacion(IdRepara, NumSerie, IdProd, EstadoAnterior, EstadoNuevo,FchCambio)
+ SELECT i.IdRepara,i.NumSerie,i.IdProd,d.StsRepara,i.StsRepara,i.FchRepara
+ FROM inserted i, deleted d 
+ where i.IdRepara = d.IdRepara and i.StsRepara in ('Terminado'  , 'Cancelado');
+END;
+
+--b. Crea un disparador llamado trg_PrevenirEliminacionReparaciones, que impida
+--la eliminación de registros en la tabla Repara si la reparación tiene el estado
+--"Terminado" o "En testing".
+--Eltrigger debe activarse cuando se intente eliminar un registro en Repara.
+--Debe permitir eliminar reparaciones solo si su estado es "Iniciado" o
+--"Cancelado".
+--Si el estado es "En testing" o "Terminado", debe bloquear la eliminación y
+--mostrar un mensaje de error.
+--Si la eliminación es permitida, debe registrarse en una tabla de auditoría
+--HistoricoEliminacionReparaciones, con los siguientes datos:
+--CREATE TABLE HistoricoEliminacionReparaciones (
+--IdHist INT IDENTITY PRIMARY KEY,
+--IdRepara INT NOT NULL,
+--NumSerie CHAR(10) NOT NULL,
+--IdProd INT NOT NULL,
+--StsRepara VARCHAR(20) NOT NULL,
+--FchEliminacion DATETIME DEFAULT GETDATE()
+--);
+CREATE TRIGGER trg_PrevenirEliminacionReparaciones
+ON Repara
+instead of DELETE
+AS
+BEGIN
+set nocount on;
+if exists (select 1 from deleted d where d.StsRepara in ('En testing', 'Terminado'))
+BEGIN
+ THROW 51000, 'No se puede eliminar porque el StsRepara esta en Testing o Terminado.', 1;  
+END
+insert into HistoricoEliminacionReparaciones(IdRepara,NumSerie,IdProd,StsRepara)
+select d.IdRepara,d.NumSerie,d.IdProd,d.StsRepara 
+from deleted d ;
+
+DELETE FROM Repara 
+WHERE IdRepara in (select IdRepara from deleted 
+where StsRepara in ('Iniciado','Cancelado'))
+END;
+
+--la tabla deleted es la tupla anterior al cambio
+--y la tabla iserted es la tupla despues del cambio
